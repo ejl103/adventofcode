33d07a08ecd81b2548cafd97fd2ee86cb1753192
ejl103 <82910232+ejl103@users.noreply.github.com>
2021-12-01 21:39:13 +0000

    Update Program.cs
    Simplified the code somewhat

--- 2021/day/1/day_one/Program.cs
+++ 2021/day/1/day_one/Program.cs
@@ -23,7 +23,7 @@ namespace day_one
             }).ToArray();
 
             // In C#10 can use var, but using 9 here to mirror Unity
-            Func<IEnumerable<int>, int> get_increase = (array) =>
+            Func<int[], int> get_increase = (array) =>
             {
                 int increase_count = 0;
                 int? previous_value = null;
@@ -42,22 +42,13 @@ namespace day_one
 
             Console.WriteLine($"Increase values: {get_increase(as_int_array)}");
 
-            var chunks = as_int_array
-            .Select((s, i) =>
-            {
-                if (i + 2 < as_int_array.Length)
+            var summed_list = as_int_array
+                .Where((s, i) => i + 2 < as_int_array.Length)
+                .Select((s, i) =>
                 {
-                    return new int[] { s, as_int_array[i + 1], as_int_array[i + 2], };
-                }
-                return null;
-            }).ToArray();
+                    return s + as_int_array[i + 1] + as_int_array[i + 2];
+                }).ToArray();
 
-            var summed_list = new List<int>(chunks.Length);
-            foreach (var triplet in chunks)
-            {
-                if (triplet is not null)
-                    summed_list.Add( triplet.Sum() );
-            }
 
             Console.WriteLine($"Increase values for three measure: {get_increase(summed_list)}");
             Console.ReadKey();
