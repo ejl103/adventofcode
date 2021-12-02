25bd0171d66f7285b66251bed879bc269eee7870
ejl103 <82910232+ejl103@users.noreply.github.com>
2021-12-02 21:19:03 +0000

    Minor simplifications to day 2

--- 2021/day_two/Program.cs
+++ 2021/day_two/Program.cs
@@ -9,8 +9,9 @@ namespace day_two
         {
             string[] lines = System.IO.File.ReadAllLines(@"..\..\..\input.txt");
 
-            int depth = 0;
-            int horizontal = 0;
+            int depth = 0, depth2 = 0;
+            int horizontal = 0, horizontal2 = 0;
+            int aim = 0;
             foreach (var line in lines)
             {
                 var instruction_move = line.Split(' ');
@@ -20,12 +21,19 @@ namespace day_two
                 {
                     case "forward":
                         horizontal += increment;
+
+                        horizontal2 += increment;
+                        depth2 += increment * aim;
                         break;
                     case "up":
                         depth -= increment;
+
+                        aim -= increment;
                         break;
                     case "down":
                         depth += increment;
+
+                        aim += increment;
                         break;
                     default:
                         throw new InvalidOperationException();
@@ -35,32 +43,7 @@ namespace day_two
             var part_one_answer = depth * horizontal;
             Console.WriteLine($"Part one answer {part_one_answer}");
 
-            depth = 0;
-            horizontal = 0;
-            int aim = 0;
-            foreach (var line in lines)
-            {
-                var instruction_move = line.Split(' ');
-                var increment = int.Parse(instruction_move[1]);
-
-                switch (instruction_move[0])
-                {
-                    case "forward":
-                        horizontal += increment;
-                        depth += increment * aim;
-                        break;
-                    case "up":
-                        aim -= increment;
-                        break;
-                    case "down":
-                        aim += increment;
-                        break;
-                    default:
-                        throw new InvalidOperationException();
-                }
-            }
-
-            var part_two_answer = depth * horizontal;
+            var part_two_answer = depth2 * horizontal2;
             Console.WriteLine($"Part two answer {part_two_answer}");
         }
     }
