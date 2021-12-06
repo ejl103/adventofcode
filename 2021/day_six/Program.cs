7d96485264ca3d31c807f8f7cd23b8ebb83d4b90
ejl103 <edward.lambert@gmail.com>
2021-12-06 11:28:36 +0000

    Day 5 part 1 & 2

---
+++ 2021/day_six/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace day_six
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"..\..\..\input.txt");
+            System.Diagnostics.Debug.Assert(lines.Length == 1);
+
+            var starting_ages = (from entry in lines[0].Split(',') select int.Parse(entry)).ToArray();
+
+            // Ages - ulong has no sum, but long is big enough
+            var ages = new long[9];
+
+            foreach (var starting_age in starting_ages)
+            {
+                ages[starting_age]++;
+            }
+
+            // As we modify need to take a copy
+            var part1_answer = Simulate(80, (long[])ages.Clone());
+
+            Console.WriteLine($"Part 1 answer: {part1_answer}");
+
+            var part2_answer = Simulate(256, ages); 
+
+            Console.WriteLine($"Part 1 answer: {part2_answer}");
+        }
+
+        static long Simulate(int days, long[] ages)
+        {
+            for (int day = 0; day < days; day++)
+            {
+                var spawned = ages[0];
+
+                // shuffle up
+                for (int i = 0; i < ages.Length - 1; i++)
+                {
+                    ages[i] = ages[i + 1];
+                }
+
+                ages[8] = spawned;
+                ages[6] += spawned;
+            }
+
+            return ages.Sum();
+        }
+    }
+}
