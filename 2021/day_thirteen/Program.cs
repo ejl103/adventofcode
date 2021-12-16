c256361023c6c37ab297b3d32ed3d81693416506
ejl103 <82910232+ejl103@users.noreply.github.com>
2021-12-16 23:41:31 +0000

    Day 13 part 2

--- 2021/day_thirteen/Program.cs
+++ 2021/day_thirteen/Program.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
 
 namespace day_thirteen
 {
     internal class Program
     {
-        struct PointComparer : IEqualityComparer<int[]>
+        struct PointComparer : IEqualityComparer<int[]>, IComparer<int[]>
         {
+            public int Compare(int[]? one, int[]? two)
+            {
+                // Sort by low y then low x
+                if (one[1] != two[1])
+                    return one[1] - two[1];
+
+                return one[0] - two[0];
+            }
+
             public bool Equals(int[]? x, int[]? y)
             {
                 if (x.Length != 2 || y.Length != 2)
@@ -22,6 +32,7 @@ namespace day_thirteen
                 return obj[0] ^ obj[1];
             }
         }
+
         static void Main(string[] args)
         {
             string[] lines = System.IO.File.ReadAllLines(@"..\..\..\input.txt");
@@ -45,31 +56,94 @@ namespace day_thirteen
                 }
             }
 
-            // part 1 just the first fold
-            bool y_fold = folds[0].line == 'y';
-
-            // [0] will be > threshold
-            // [1] will be < 
-            var grouped = coords.GroupBy(c => y_fold ? c[1] < folds[0].index : c[0] < folds[0].index).Select(s => s.Select(v => v).ToList()).ToArray();
-
-            int index = y_fold ? 1 : 0;
-
-            // Fold up so higher numbers need adjust to match the the lower "half" coords
-            foreach (var pair in grouped[0])
+            bool part1_done = false;
+            foreach (var fold in folds)
             {
-                int new_coord = folds[0].index - (pair[index] - folds[0].index);
-                System.Diagnostics.Debug.Assert(new_coord >= 0);
+                bool y_fold = fold.line == 'y';
 
-                var new_coords = new int[2];
-                new_coords[index] = new_coord; 
-                new_coords[1 - index] = pair[1 - index];
+                // can't predict which side the > group will be
+                var grouped = coords.GroupBy(c => y_fold ? c[1] < fold.index : c[0] < fold.index).Select(s => s.Select(v => v).ToList()).ToList();
 
-                grouped[1].Add(new_coords);
+                int greater_than_group;
+
+                // Nothing to fold??
+                if (grouped.Count < 2)
+                {
+                    //continue;
+                    // If only one fold need to figure out which way round it is
+                    if ((y_fold && grouped[0][0][1] > fold.index) || (!y_fold && grouped[0][0][0] > fold.index))
+                    {
+                        grouped.Add(new());
+                        greater_than_group = 1;
+                    }
+                    else
+                        continue;
+                }
+                else
+                {
+                    greater_than_group = (y_fold && grouped[0][0][1] > fold.index) || (!y_fold && grouped[0][0][0] > fold.index) ? 0 : 1;
+                }
+
+                var less_than_group = 1 - greater_than_group;
+                int index = y_fold ? 1 : 0;
+
+                // Fold up so higher numbers need adjust to match the the lower "half" coords
+                foreach (var pair in grouped[greater_than_group])
+                {
+                    int new_coord = fold.index - (pair[index] - fold.index);
+                    System.Diagnostics.Debug.Assert(new_coord >= 0);
+
+                    var new_coords = new int[2];
+                    new_coords[index] = new_coord;
+                    new_coords[1 - index] = pair[1 - index];
+
+                    grouped[less_than_group].Add(new_coords);
+                }
+
+                coords = grouped[less_than_group].Distinct(new PointComparer()).ToList();
+
+                if (!part1_done)
+                {
+                    // part 1 just the first fold
+                    Console.WriteLine($"Part one answer: {coords.Count}");
+                    part1_done = true;
+                }
+
+
+            }
+            Console.WriteLine($"Part two answer will be written to grid ");
+
+            coords.Sort(new PointComparer());
+
+            using StreamWriter file = new("part2.txt", append: false);
+            
+            int prev_y = 0;
+            int prev_x = 0;
+            for (int i = 0; i < coords.Count; i++)
+            {
+                int y = coords[i][1];;
+                if (y != prev_y)
+                {
+
+                    // lines with nothing on them
+                    for (int j = prev_y; j < y; j++)
+                    {
+                        file.Write('\n');
+                    }
+                    prev_y = y;
+                    prev_x = 0;
+                }
+
+                int x = coords[i][0];
+                for (int j = prev_x; j < x; j++)
+                {
+                    file.Write(' ');
+                }
+                prev_x = x+1;
+
+                file.Write('*');
             }
 
-            var distinct_points = grouped[1].Distinct(new PointComparer()).ToArray();
-
-            Console.WriteLine($"Part one answer: {distinct_points.Length}");
         }
     }
 }
